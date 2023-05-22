cd8ca66b0413588342564d66e0fe57577ef30bf2
Lucas Ferreira <lucas6rodrigues@yahoo.com.br>
2023-05-22 11:27:38 -0300

    feat: Controller de Category e Transactions
    Implementação dos controllers de categorias e de transações

---
+++ LevvaCoin/LevvaCoinAPI/Controllers/CategoryControllers.cs
@@ -0,0 +1,47 @@
+﻿using LevvaCoinAPI.Logic.Dto;
+using LevvaCoinAPI.Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LevvaCoinAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _CategoryService;
+        public CategoryController(ICategoryService service)
+        {
+            _CategoryService = service;
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(CategoryDto category)
+        {
+            _CategoryService.Create(category);
+            return Created("", category);
+        }
+
+        [HttpGet("Get")]
+        public ActionResult<CategoryDto> Get(int id)
+        {
+            return _CategoryService.Get(id);
+        }
+
+        [HttpPut("Att")]
+        public IActionResult Update(CategoryDto category)
+        {
+            _CategoryService.Update(category);
+            return Ok(category);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            _CategoryService.Delete(id);
+            return Ok();
+        }
+
+
+    }
+
+}
---
+++ LevvaCoin/LevvaCoinAPI/Controllers/TransactionController.cs
@@ -0,0 +1,47 @@
+﻿using LevvaCoinAPI.Logic.Dto;
+using LevvaCoinAPI.Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LevvaCoinAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _TransactionService;
+        public TransactionController(ITransactionService service)
+        {
+            _TransactionService = service;
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(TransactionDto transaction)
+        {
+            _TransactionService.Create(transaction);
+            return Created("", transaction);
+        }
+
+        [HttpGet("Get")]
+        public ActionResult<TransactionDto> Get(int id)
+        {
+            return _TransactionService.Get(id);
+        }
+
+        [HttpPut("Att")]
+        public IActionResult Update(TransactionDto transaction)
+        {
+            _TransactionService.Update(transaction);
+            return Ok(transaction);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            _TransactionService.Delete(id);
+            return Ok();
+        }
+
+
+    }
+
+}
